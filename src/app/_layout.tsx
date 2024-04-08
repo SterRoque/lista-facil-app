d0f0aed6335e6f57bf62ad271b7799db877f9bc8
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-04-07 23:50:31 -0300

    fix: status bar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,11 +3,13 @@ import { theme } from 'constants/theme';
 import { Slot } from 'expo-router';
 import { PaperProvider } from 'react-native-paper';
 import { PreloaderContextProvider } from 'contexts/PreloaderContext';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Layout() {
    return (
       <PaperProvider theme={theme}>
          <PreloaderContextProvider>
+            <StatusBar style='dark' />
             <Slot />
          </PreloaderContextProvider>
       </PaperProvider>
